6b5e7e7973a95418026549518d30c716edf18fec
Harley Johnson <62576957+JohnsonHarleyR@users.noreply.github.com>
2025-03-21 23:25:56 +0000

    Fixed it so it works now.

--- utility.js
+++ utility.js
@@ -1,14 +1,14 @@
 import { createClient } from 'https://cdn.skypack.dev/@supabase/supabase-js@2.7.1';
 
 async function getMembers() {
-    const supabaseUrl = 'https://xbjqfchadtujvvygkrui.supabase.co';
+    const supabaseUrl = 'https://xbjqfchadtujvvygkrui.supabase.co'
     const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhianFmY2hhZHR1anZ2eWdrcnVpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDI1OTc3MzYsImV4cCI6MjA1ODE3MzczNn0.28kUpBvmKOzrwaVpI9yA7EiqaexZic1erElczZRyPqg';
     const supabase = createClient(supabaseUrl, supabaseKey);
 
     let { data: members, error } = await supabase
-    .from('members')
-    .select('*')
-
+  .from('members')
+  .select('*')
+    console.log(members);
     let tableBody = document.getElementById('member-rows');
     for (let member of members) {
         tableBody.innerHTML += `<tr>
